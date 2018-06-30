c0f49629884b4602cdc251cd5cb29a9541f8e74b
Igor Rodrigues <igor.chagas.rodrigues@gmail.com>
2018-06-30 15:01:54 -0300

    Cap 1

---
+++ src/main/java/br/com/alura/loja/modelo/Projeto.java
@@ -0,0 +1,41 @@
+package br.com.alura.loja.modelo;
+
+import com.thoughtworks.xstream.XStream;
+
+public class Projeto {
+	
+	private long id;
+	private String nome;
+	private int anoDeInicio;
+	
+	public Projeto() {
+		
+	}
+	
+	public Projeto(long id, String nome, int anoDeInicio) {
+		this.id = id;
+		this.nome = nome;
+		this.anoDeInicio = anoDeInicio;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getAnoDeInicio() {
+		return anoDeInicio;
+	}
+	
+	public String toXML() {
+		return new XStream().toXML(this);
+	}
+	
+}
